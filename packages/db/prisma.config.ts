62756123efe6685bab3e7dfac78168d05f76c5b1
Ayush Sonawale <75654165+ayushsonawale@users.noreply.github.com>
2025-11-14 23:56:00 +0530

    db add

---
+++ packages/db/prisma.config.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" }); // Load env manually
+
+import { defineConfig } from "prisma/config";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL missing in .env");
+}
+
+export default defineConfig({
+  schema: "./prisma/schema.prisma",
+  migrations: {
+    path: "./prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
